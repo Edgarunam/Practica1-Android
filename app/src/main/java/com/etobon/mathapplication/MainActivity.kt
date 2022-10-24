58014d39913f9f271779b3d187e313100aeab130
Edgarunam <etobon96@gmail.com>
2022-10-24 18:27:35 -0500

    Splash with Delay

--- app/src/main/java/com/etobon/mathapplication/MainActivity.kt
+++ app/src/main/java/com/etobon/mathapplication/MainActivity.kt
@@ -9,19 +9,13 @@ import android.view.WindowManager
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN
-        )
         setContentView(R.layout.activity_main)
-
-        Handler().postDelayed({
-            val i = Intent(
-                this@MainActivity,
-                MainActivity2::class.java
-            )
-            startActivity(i)
+        Thread{
+            Thread.sleep(3000)
+            val intent = Intent(this,MainActivity2::class.java)
+            startActivity(intent)
             finish()
-        }, 2000)
+        }
+        
     }
 }
