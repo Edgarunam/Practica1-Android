a49069aa158826a27b8bd6d0073a0dfbf44cd2ed
Edgarunam <etobon96@gmail.com>
2022-10-24 19:42:28 -0500

    Spinner add

--- app/src/main/java/com/etobon/mathapplication/MainActivity.kt
+++ app/src/main/java/com/etobon/mathapplication/MainActivity.kt
@@ -5,17 +5,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.view.WindowManager
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Thread{
-            Thread.sleep(3000)
-            val intent = Intent(this,MainActivity2::class.java)
+        thread{
+            Thread.sleep(1000)
+            val intent = Intent(this, MainActivity2::class.java)
             startActivity(intent)
             finish()
         }
-        
+
+
+
+
     }
 }
--- app/src/main/java/com/etobon/mathapplication/MainActivity2.kt
+++ app/src/main/java/com/etobon/mathapplication/MainActivity2.kt
@@ -2,10 +2,36 @@ package com.etobon.mathapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 class MainActivity2 : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        val fragment = SpinnerFragment()
+        fragmentTransaction.add(R.id.MainActivity2,fragment).commit()
+
+
+// Create an ArrayAdapter using the string array and a default spinner layout
+        val spinner: Spinner = findViewById(R.id.SpinnerFormulas)
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.planets_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinner.adapter = adapter
+        }
+
+
+
+
     }
 }
