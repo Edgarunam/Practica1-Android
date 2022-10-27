90a96d4f76baf4bc7ca812d4493c75d31e32176e
Edgarunam <etobon96@gmail.com>
2022-10-26 21:45:04 -0500

    Densidad-Ready

--- app/src/main/java/com/etobon/mathapplication/Fragment1.kt
+++ app/src/main/java/com/etobon/mathapplication/Fragment1.kt
@@ -5,17 +5,16 @@ import android.text.Editable
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
-import android.view.View.inflate
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.EditText
-import android.widget.Spinner
 import android.widget.Toast
 import androidx.core.graphics.toColor
 import androidx.fragment.app.FragmentActivity
+import com.etobon.mathapplication.databinding.ActivityMain2Binding.bind
 import com.etobon.mathapplication.databinding.ActivityMain2Binding.inflate
 import com.etobon.mathapplication.databinding.FragmentBlankBinding
 import com.etobon.mathapplication.databinding.FragmentBlankBinding.inflate
+import java.text.DecimalFormat
 
 
 class Fragment1 : Fragment(){
@@ -36,14 +35,57 @@ class Fragment1 : Fragment(){
 
     override fun onStart() {
         super.onStart()
+
+
+
         binding.button2.setOnClickListener {
-            binding.volumentv.text = "Hola desde Fragment"
+
+            var Status: Int = 0
+            if (binding.etMasa.text.isEmpty() && binding.etVolumen.text.isNotEmpty()) {
+                binding.etMasa.setBackgroundColor(10)
+
+                Status = 1
+
+            } else if (binding.etVolumen.text.isEmpty() && binding.etMasa.text.isNotEmpty()) {
+
+                Status = 2
+            }
+            else if (binding.etVolumen.text.isEmpty() && binding.etMasa.text.isEmpty()) {
+
+                Status = 3
+            }
+
+            when (Status) {
+                1 -> {binding.etMasa.hint = "Ingrese la Masa"}
+                2 -> {binding.etVolumen.hint = "Ingrese el Volumen"}
+                3 -> {binding.etMasa.hint = "Ingrese la Masa"
+                      binding.etVolumen.hint = "Ingrese el Volumen"}
+                else -> {
+                    var masa: Float = binding.etMasa.text.toString().toFloat()
+                    var volumen:Float = binding.etVolumen.text.toString().toFloat()
+                    binding.Answertv.text = Density(masa,volumen).toString()
+                }
+
+
+            }
+
+
         }
     }
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
+
+
+        override fun onDestroyView() {
+            super.onDestroyView()
+            _binding = null
+        }
+
+        fun Density(masaInput: Float, volumenInput: Float): Float {
+            return masaInput / volumenInput
+
+            }
+
+
+
 }
 
 
